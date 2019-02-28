5ed02dc467915c42c0d71b7e0d4ff0295952178a
Karlafgl <karlafgl@gmail.com>
2019-02-28 10:14:33 -0700

    adding local store functions

--- appes6.js
+++ appes6.js
@@ -58,23 +58,50 @@ class UI {
 //Local Storage Class
 class Store{
     static getBooks() {
-
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = []
+        } else {
+            books = JSON.parse(localStorage.getItem('books'))
+        }
+        return books
     }
     
     static displayBooks() {
+        const books = Store.getBooks()
 
+        books.forEach(function(book){
+            const ui = new UI;
+            //Add book to UI
+            ui.addBookToList(book)
+        })
     }
 
     static addBook(book) {
-        
+        const books = Store.getBooks()
+
+        books.push(book)
+
+        localStorage.setItem('books', JSON.stringify(books))
     }
 
-    static removeBook() {
+    static removeBook(isbn) {
+        console.log(isbn)
+        const books = Store.getBooks()
 
+        books.forEach((book, index) => {
+            if(book.isbn === isbn) {
+                books.splice(index, 1)
+            }
+        })
+
+        localStorage.setItem('books', JSON.stringify(books))
     }
 
 
 }
+//DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks)
 
 //Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
@@ -118,6 +145,9 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
     //delete book
     ui.deleteBook(e.target)
+
+    //Delete from Ls
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
     //Show message
     ui.showAlert('Book removed!', 'success')
